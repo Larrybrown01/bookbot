186493e867331f82264bd596bb832563a3355978
VMProvoost <vincent.provoost@student.hu.nl>
2024-09-03 17:46:56 +0200

    added read book frankenstein

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print('hello world') 
-# added a comment as well. this blows.
+def main():
+    book_path = "bookbot/books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
+
