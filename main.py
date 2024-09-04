402e604f81c383aa2cb775658eabc839127b3006
VMProvoost <vincent.provoost@student.hu.nl>
2024-09-04 14:30:23 +0200

    finished bookbot

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
-# from collections import Counter
-
 def main():
     book_path = "bookbot/books/frankenstein.txt"
     text = get_book_text(book_path)
-    # num_words = get_num_words(text)
-    # print(f"{num_words} words found in the document")
+    num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    dictionary_keys = get_dict_key_value(chars_dict)
-    print(dictionary_keys)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
-def get_dict_key_value(dictionary):
-    keys = []
-    values = []
-    sorted = dictionary.sort(reverse = True)
-    items = sorted.items()
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
 
-    for i in items:
-        keys.append(i[0]), values.append(i[1])
-        print(f"The {str(keys)} character was found {str(values)} times")
-        keys = []
-        values = []
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
 
 def get_chars_dict(text):
     chars = {}
@@ -32,80 +45,9 @@ def get_chars_dict(text):
     return chars
 
 
-def get_num_words(text):
-    words = text.split()
-    return len(words)
-
-
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 
 main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def main():
-#     number_of_words = 0
-    
-#     book_path = "bookbot/books/frankenstein.txt"
-#     data_uitlezen = get_book_text(book_path)
-#     woorden_splitsen = data_uitlezen.split()
-#     number_of_words += len(woorden_splitsen)
-#     print(number_of_words)
-    # lowercase = text.lower()
-    # dictionary = Counter(lowercase)
-    # print(f'{dictionary}')
-   
-
-
-# #def print_text():
-#   #  book_path = "bookbot/books/frankenstein.txt"
-#   #  text = get_book_text(book_path)
-#   #  print(text)
-
-# def get_book_text(path):
-#     with open(path) as f:
-#         return f.read()
-
-# def word_count(book):
-#     words = book.split(" ")
-#     return Counter(words)
-
-
-
-# main()
-
-# with open(r"/Users/vincentprovoost/workspace/bookbot/books/frankenstein.txt",'r') as file:
-#     data = file.read()
-#     lines = data.split()
-#     number_of_words += len(lines)
- 
-# print(number_of_words)
